c508810e9cc942aece620f7a1799b62d49be980a
daseul.kim <da91love@gmail.com>
2020-09-13 21:36:26 +0900

    [update]apply mdb

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {
+  MDBBtn,
+} from 'mdbreact';
 
 const Main = () => (
   <div>
-    This is a main page
+    <MDBBtn color="primary">Primary</MDBBtn>
+    <MDBBtn>Default</MDBBtn>
+    <MDBBtn color="secondary">Secondary</MDBBtn>
+    <MDBBtn color="success">Success</MDBBtn>
+    <MDBBtn color="info">Info</MDBBtn>
+    <MDBBtn color="warning">Warning</MDBBtn>
+    <MDBBtn color="danger">Danger</MDBBtn>
   </div>
 );
 
