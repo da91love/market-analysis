95cd0243a734b381c5c9fa57905ce18b7706bb8e
daseul.kim <da91love@gmail.com>
2021-08-08 14:57:13 +0900

    [update]doing valuation remove

--- src/consts/api.js
+++ src/consts/api.js
@@ -10,5 +10,21 @@ export const API = {
     GET_BOOKMARK: {
         METHOD: 'get',
         URL: 'http://192.168.8.102:8081/dev/api/ma-api/v1/front/bookmark',
+    },
+    PUT_VALUATION: {
+        METHOD: 'put',
+        URL: 'http://192.168.8.102:8081/dev/api/ma-api/v1/front/valuation',
+    },
+    GET_VALUATION: {
+        METHOD: 'get',
+        URL: 'http://192.168.8.102:8081/dev/api/ma-api/v1/front/valuation',
+    },
+    PUT_COMP_TG_GRP: {
+        METHOD: 'put',
+        URL: 'http://192.168.8.102:8081/dev/api/ma-api/v1/front/comp-tg-grp',
+    },
+    GET_COMP_TG_GRP: {
+        METHOD: 'get',
+        URL: 'http://192.168.8.102:8081/dev/api/ma-api/v1/front/comp-tg-grp',
     }
 }
