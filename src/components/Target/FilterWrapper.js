687baa81a6167cc99ef8c16193d00bdda57ecb73
daseul.kim <da91love@gmail.com>
2022-03-18 15:08:58 +0900

    [update]fixing filters

--- src/components/Target/FilterWrapper.js
+++ src/components/Target/FilterWrapper.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import _ from "lodash";
+import axios from 'axios';
 import {useTranslation} from "react-i18next";
 
 import ShareDataContext from "../../contexts/ShareDataContext";
@@ -16,63 +17,88 @@ import MvTimesFilter from '../Share/MvTimesFilter';
 
 import { MODELS } from '../../consts/model';
 import { KEY_NAME } from '../../consts/keyName';
+import {API} from '../../consts/api';
+
 
 const FilterWrapper = (props) => {
   const {model, filterStatus, setFilterStatus} = props;
+  const {country} = useContext(ShareDataContext);
   const { t } = useTranslation();
   const [mdlFilterStatus, setMdlFilterStatus] = useState(null);
-  const {yearRawData, quarterRawData} = useContext(ShareDataContext);
-
-  useEffect(() => {
-    setMdlFilterStatus(filterStatus?.[model])
-  },[model])
+  const [filtersByModel, setFiltersByModel] = useState([]);
 
   const saveHandler = () => {
     setFilterStatus({...filterStatus, [model]:mdlFilterStatus});
-  }
+  };
 
-  const getInputsByModel = () => {
-    const uniqYearPeriods = _.uniq(yearRawData.map((v,i)=> v[KEY_NAME.PERIOD])).sort().reverse();
-    const uniqQuarterPeriods = _.uniq(quarterRawData.map((v,i)=> v[KEY_NAME.PERIOD])).sort().reverse();
+  useEffect(() => {
+    if (model!=="default") {
+      axios({
+        method: API.POST_FINANCIAL_SUMMARY.METHOD,
+        url: API.POST_FINANCIAL_SUMMARY.URL,
+        data: {
+          data: {
+            country: country,
+            displayCol: [KEY_NAME.PERIOD]
+          }
+        }
+      })
+        .then(res => {
+            if(res.data.status === "success" ) {
+                const {year_result, quarter_result} = res.data.payload.value;
+                const yearUniqPeriods = _.uniq(year_result.map(x => x[KEY_NAME.PERIOD])).sort().reverse();
+                const quarterUniqPeriods = _.uniq(quarter_result.map(x => x[KEY_NAME.PERIOD])).sort().reverse();
 
-    const inputs = [];
-    if (model === MODELS.VALUE) {
-      // TODO : add periodFilter
-      inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<PerFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<RoeFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-    } else if (model == MODELS.TURNAROUND) {
-      inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<ProfitTypeFilter options={[KEY_NAME.OP, KEY_NAME.EBITDA, KEY_NAME.NP_CTRL]} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<TermFilter append={"quarters"} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-    } else if (model === MODELS.CPGROWTH) {
-      inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<TermFilter append={"quarters"} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<OpTimesFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-    } else if (model === MODELS.MRKGROWTH) {
-      inputs.push(<PeriodFilter options={uniqYearPeriods} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<MvTimesFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-    } else if (model === MODELS.COLLAPSE) {
-      inputs.push(<PeriodFilter options={uniqYearPeriods} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<TermFilter append={"years"} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<MvTimesFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-    } else if (model === MODELS.BLUECHIP) {
-      inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<SalesFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<PerFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<RoeFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-    } else if (model === MODELS.INVGROWTH) {
-      inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
-      inputs.push(<IaCfTimesFilter mdlFilterStatus={mdlFilterStatus} setMdlFilterStatus={setMdlFilterStatus}/>);
+                const tempMdlFilterStatus = filterStatus?.[model];
+                setMdlFilterStatus(tempMdlFilterStatus);
+                setFiltersByModel(getInputsByModel(yearUniqPeriods, quarterUniqPeriods, tempMdlFilterStatus, setMdlFilterStatus));
+
+            } else {
+                // enqueueSnackbar(`${MSG.LOGIN_FAIL}`, {variant: ERROR});
+            }
+        })
     }
+}, [model]);
 
-    return inputs;
+const getInputsByModel = (uniqYearPeriods, uniqQuarterPeriods, filterStatus, setFilterStatus) => {
+  const inputs = [];
+  if (model === MODELS.VALUE) {
+    // TODO : add periodFilter
+    inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<PerFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<RoeFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+  } else if (model == MODELS.TURNAROUND) {
+    inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<ProfitTypeFilter options={[KEY_NAME.OP, KEY_NAME.EBITDA, KEY_NAME.NP_CTRL]} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<TermFilter append={"quarters"} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+  } else if (model === MODELS.CPGROWTH) {
+    inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<TermFilter append={"quarters"} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<OpTimesFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+  } else if (model === MODELS.MRKGROWTH) {
+    inputs.push(<PeriodFilter options={uniqYearPeriods} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<MvTimesFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+  } else if (model === MODELS.COLLAPSE) {
+    inputs.push(<PeriodFilter options={uniqYearPeriods} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<TermFilter append={"years"} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<MvTimesFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+  } else if (model === MODELS.BLUECHIP) {
+    inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<SalesFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<PerFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<RoeFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+  } else if (model === MODELS.INVGROWTH) {
+    inputs.push(<PeriodFilter options={uniqQuarterPeriods} mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
+    inputs.push(<IaCfTimesFilter mdlFilterStatus={filterStatus} setMdlFilterStatus={setFilterStatus}/>);
   }
+
+  return inputs;
+}
 
   return (
     <>
         {model!=="default"?
-        <FilterModalBtn filterComponent={getInputsByModel()} saveHandler={saveHandler}/>
+        <FilterModalBtn filterComponent={filtersByModel || null} saveHandler={saveHandler}/>
         :<FilterModalBtn filterComponent={null} />}
     </>
   )
